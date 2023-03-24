2b92084dba65305d60525fdd185022a5e384a5e0
2003Hamza <128781459+2003Hamza@users.noreply.github.com>
2023-03-24 12:29:42 +0600

    Add files via upload

---
+++ index46.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hamza</title>
+</head>
+<body>
+    <?php 
+    
+        //else if 
+
+        $mont_name = "jun";
+
+        if ( $mont_name == "jan"){
+            echo "janouary";
+        }else if ( $mont_name == "feb"){
+            echo "febuary";
+        } else if ( $mont_name == "mar"){
+            echo "march";
+        } else if ( $mont_name == "apr"){
+            echo "april";
+        } else if ( $mont_name == "mey"){
+            echo "may";
+        } else if ( $mont_name == "jun"){
+            echo "june";
+        } 
+    
+    
+    
+    ?>
+</body>
+</html>
