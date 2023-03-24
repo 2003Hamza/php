40c1243725c1c7756a358f685208417d2c090f3b
2003Hamza <128781459+2003Hamza@users.noreply.github.com>
2023-03-24 12:27:31 +0600

    Add files via upload

---
+++ index42.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hamza </title>
+</head>
+<body>
+    <?php 
+    
+        // concatenation operator 
+    
+        // $first_name = "Amir";
+         //$last_name = "Hamza";
+
+        // $ful_name = "Hello"." ".$first_name ." ". $last_name ;
+
+        // echo $ful_name ;
+    
+      $first_name  = "Amir";
+
+      $first_name .= " Hamza";
+
+      echo $first_name ;
+    
+    ?>
+</body>
+</html>
